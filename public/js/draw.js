5c01beafcb322fdd5af7b588ac43aa13864a5378
Jeremy Keeler <jj.of.keeler@gmail.com>
2015-03-04 08:44:12 -0500

    get EaselJS working. DOES not work cross people yet

--- public/js/draw.js
+++ public/js/draw.js
@@ -1,59 +1,63 @@
 
-  var canvas, stage;
-  var drawingCanvas;
-  var oldPt;
-  var oldMidPt;
-  var title;
-  var color;
-  var stroke;
-  var colors;
-  var index;
-  var startDraw = function() {
-    console.log("Hello from draw!");
-    canvas = document.getElementById("#gameCanvas");
-    index = 0;
-    colors = ["#828b20", "#b0ac31", "#cbc53d", "#fad779", "#f9e4ad", "#faf2db", "#563512", "#9b4a0b", "#d36600", "#fe8a00", "#f9a71f"];
-    //check to see if we are running in a browser with touch support
-    stage = new createjs.Stage(canvas);
-    stage.autoClear = false;
-    stage.enableDOMEvents(true);
-    createjs.Touch.enable(stage);
-    createjs.Ticker.setFPS(24);
-    drawingCanvas = new createjs.Shape();
-    stage.addEventListener("stagemousedown", handleMouseDown);
-    stage.addEventListener("stagemouseup", handleMouseUp);
-    title = new createjs.Text("Click and Drag to draw", "36px Arial", "#777777");
-    title.x = 300;
-    title.y = 200;
-    stage.addChild(title);
-    stage.addChild(drawingCanvas);
-    stage.update();
-    console.log("Hello again");
-  }
+var canvas, stage;
+var drawingCanvas;
+var oldPt;
+var oldMidPt;
+var title;
+var color;
+var stroke;
+var colors;
+var index;
 
-  var handleMouseDown = function(event) {
-    if (!event.primary) { return; }
-    if (stage.contains(title)) {
-      stage.clear();
-      stage.removeChild(title);
-    }
-    color = colors[(index++) % colors.length];
-    stroke = Math.random() * 30 + 10 | 0;
-    oldPt = new createjs.Point(stage.mouseX, stage.mouseY);
-    oldMidPt = oldPt.clone();
-    stage.addEventListener("stagemousemove", handleMouseMove);
+var startDraw = function() {
+  console.log("Hello from draw!");
+  canvas = $("#gameCanvas").get(0);
+  index = 0;
+  colors = ["#828b20", "#b0ac31", "#cbc53d", "#fad779", "#f9e4ad", "#faf2db", "#563512", "#9b4a0b", "#d36600", "#fe8a00", "#f9a71f"];
+  //check to see if we are running in a browser with touch support
+  stage = new createjs.Stage(canvas);
+  console.log(canvas);
+  stage.autoClear = false;
+  stage.enableDOMEvents(true);
+  createjs.Touch.enable(stage);
+  createjs.Ticker.setFPS(24);
+  drawingCanvas = new createjs.Shape();
+  stage.addEventListener("stagemousedown", handleMouseDown);
+  stage.addEventListener("stagemouseup", handleMouseUp);
+  title = new createjs.Text("Click and Drag to draw", "36px Arial", "#777777");
+  title.x = 300;
+  title.y = 200;
+  stage.addChild(title);
+  stage.addChild(drawingCanvas);
+  stage.update();
+  console.log("Hello again");
+}
+
+var handleMouseDown = function(event) {
+  if (!event.primary) { return; }
+  if (stage.contains(title)) {
+    stage.clear();
+    stage.removeChild(title);
   }
-  var handleMouseMove = function(event) {
-    if (!event.primary) { return; }
-    var midPt = new createjs.Point(oldPt.x + stage.mouseX >> 1, oldPt.y + stage.mouseY >> 1);
-    drawingCanvas.graphics.clear().setStrokeStyle(stroke, 'round', 'round').beginStroke(color).moveTo(midPt.x, midPt.y).curveTo(oldPt.x, oldPt.y, oldMidPt.x, oldMidPt.y);
-    oldPt.x = stage.mouseX;
-    oldPt.y = stage.mouseY;
-    oldMidPt.x = midPt.x;
-    oldMidPt.y = midPt.y;
-    stage.update();
-  }
-  var handleMouseUp = function(event) {
-    if (!event.primary) { return; }
-    stage.removeEventListener("stagemousemove", handleMouseMove);
-  }
+  color = colors[(index++) % colors.length];
+  stroke = Math.random() * 30 + 10 | 0;
+  oldPt = new createjs.Point(stage.mouseX, stage.mouseY);
+  oldMidPt = oldPt.clone();
+  stage.addEventListener("stagemousemove", handleMouseMove);
+}
+
+var handleMouseMove = function(event) {
+  if (!event.primary) { return; }
+  var midPt = new createjs.Point(oldPt.x + stage.mouseX >> 1, oldPt.y + stage.mouseY >> 1);
+  drawingCanvas.graphics.clear().setStrokeStyle(stroke, 'round', 'round').beginStroke(color).moveTo(midPt.x, midPt.y).curveTo(oldPt.x, oldPt.y, oldMidPt.x, oldMidPt.y);
+  oldPt.x = stage.mouseX;
+  oldPt.y = stage.mouseY;
+  oldMidPt.x = midPt.x;
+  oldMidPt.y = midPt.y;
+  stage.update();
+}
+
+var handleMouseUp = function(event) {
+  if (!event.primary) { return; }
+  stage.removeEventListener("stagemousemove", handleMouseMove);
+}
