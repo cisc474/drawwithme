5f6e215e31e46721a911f01d21fbfd7473dc9bdd
Jeremy Keeler <jj.of.keeler@gmail.com>
2015-03-03 22:09:10 -0500

    Add a basic game view.

--- public/js/drawwithme.js
+++ public/js/drawwithme.js
@@ -6,7 +6,10 @@ app.config(["$routeProvider", "$locationProvider",
   function($routeProvider, $locationProvider) {
 
     // The default page to be shown when a client connects
-    $routeProvider.when("/home", {
+    $routeProvider.when("/game/:id", {
+      templateUrl: "/html/game.html",
+      controller: "GameController"
+    }).when("/home", {
       templateUrl: "/html/home.html",
       controller: "HomeController"
     }).otherwise({
@@ -21,5 +24,21 @@ var socket = io.connect();
 app.controller("HomeController", ["$scope", 
   function($scope) {
 
+    // Function called when a player wishes to join a game
+    $scope.joinGame = function() {
+      console.log("Joining a game...");
+    };
+  }
+]);
+
+// Theis controller controls the game view
+app.controller("GameController", ["$scope", "$routeParams" 
+  function($scope, $routeParams) {
+    $scope.messages = [];
+
+    // What to do when sending a message
+    $scope.sendMessage = function() {
+      $scope.text = "";
+    };
   }
 ]);
